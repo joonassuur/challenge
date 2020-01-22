6922bc01f63791569ec62921c2f7707c00703962
Joonas <joonas.suurvali@gmail.com>
2020-01-22 19:15:23 +0100

    Updates

---
+++ precache-manifest.e8263e3f80dcd99a33f6001b4ebf1e25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32a62480076bb60b37575fc617d29014",
+    "url": "/coding-challenge/index.html"
+  },
+  {
+    "revision": "80b4ef6dd8928c9f710a",
+    "url": "/coding-challenge/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7cb83cf17ffc0f6d78d4",
+    "url": "/coding-challenge/static/js/2.b5d431bc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/coding-challenge/static/js/2.b5d431bc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "80b4ef6dd8928c9f710a",
+    "url": "/coding-challenge/static/js/main.1e1f24c2.chunk.js"
+  },
+  {
+    "revision": "e64941cd7a37b0680138",
+    "url": "/coding-challenge/static/js/runtime-main.0ff9c853.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/coding-challenge/precache-manifest.e8263e3f80dcd99a33f6001b4ebf1e25.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/coding-challenge/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
