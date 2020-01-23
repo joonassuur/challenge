df59204779d3d46d54078d0f64bc26df4b22a9f1
Joonas <joonas.suurvali@gmail.com>
2020-01-23 08:34:09 +0100

    Added abort fetch controller

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,16 +6,29 @@ let interval;
 export default function Search(props) {
 
     const [searchInput, setSearchInput] = useState('');
+    const [isFetching, setFetching] = useState(false);
+
     const prevInputRef = useRef();
 
+    // Abort fetch instance.
+    const controller = new AbortController()
+    const signal = controller.signal
+
+    const abortFetching = () => {
+        controller.abort()
+        setFetching(false)
+    }
 
     //make the network request
     const makeReq = () => {
+
+        setFetching(true)
 
         const data = {"requests":[{"indexName":"ikea","params":`query=${searchInput}&hitsPerPage=16`}]};
 
         fetch('https://latency-dsn.algolia.net/1/indexes/*/queries?x-algolia-api-key=6be0576ff61c053d5f9a3225e2a90f76&x-algolia-application-id=latency', {
             method: 'POST', 
+            signal: signal,
             headers: {
                 'Content-Type': 'application/json',
             },
@@ -25,12 +38,16 @@ export default function Search(props) {
         .then((data) => {
             //send response data to the parent. if input field is empty, send null
             searchInput.length > 0 ? props.handleResponse(data.results[0].hits) : props.handleResponse(null)
+            setFetching(false)
         })
         .catch((error) => {
             console.error('Error:', error);
-        });
+            setFetching(false)
+        })
     }
     
+
+
     //call the network request function 200 ms after last input
     const handleInterval = async () => {     
 
@@ -42,15 +59,21 @@ export default function Search(props) {
 
     }
     
-    //compare previous state to current, then call the interval handler
+    //compare previous state to current, then call the interval handler or abort fetch
     const prevInput = prevInputRef.current;
     
     useEffect(() => {
         prevInputRef.current = searchInput
 
+        if(isFetching) {
+            abortFetching()
+        }
+
         if( prevInput !== searchInput ) {
             handleInterval()
         }
+
+
     })
 
     //assign input values to state
